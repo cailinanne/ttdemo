fc90cd775b6b1f29fa69b87ed6d8cb9e662a69f3
cailinanne <cailin.nelson@gmail.com>
2012-03-11 11:37:36 -0400

    Persist rooms.  Put users in a room.

--- chatdemo.py
+++ chatdemo.py
@@ -73,11 +73,22 @@ class MainHandler(BaseHandler):
     # messages (where N = MessageMixin.cache_size)
     def get(self):
         logging.info(self.current_user)
+        self.enter_room()
+        self.render("index.html", messages=MessageMixin.cache)
+
+
+    def enter_room(self):
+
         if self.db.users.find_one({"first_name" : self.current_user["first_name"]}) == None:
             self.db.users.insert(self.current_user)
 
-        self.render("index.html", messages=MessageMixin.cache)
+        # TODO: Let users enter different rooms
+        room = self.db.rooms.find_one({"name" : "demoroom"})
 
+        if not self.current_user["first_name"] in room["users"]:
+            self.db.rooms.update( { "name" : "demoroom" }, { "$push": { "users" : self.current_user["first_name"] } } );
+
+        self.db.users.update({"first_name" : self.current_user["first_name"]},  {"$set": {"room": room["name"]}})
 
 
 class MessageNewHandler(BaseHandler, MessageMixin):
